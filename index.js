5263bc8a5b9860304e04ddfe8a9838b40664a746
Laura Kankaanpaa <kankaanpaa.laura@hotmail.com>
2020-01-28 17:08:01 +0200

    server started and words routes created

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
+const words = require('./routes/words');
+
+app.use('/api/words', words);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 

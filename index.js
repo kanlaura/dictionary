55ad327c64eabbbd67e01d9fc22033d8d1a9fa0b
Laura Kankaanpaa <kankaanpaa.laura@hotmail.com>
2020-01-28 17:03:07 +0200

    new files created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+
+
+
+
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Kuuntelee porttia ${PORT}`)
+})
